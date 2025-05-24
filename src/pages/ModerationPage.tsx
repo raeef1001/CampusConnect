d33aa41f2982b3a8f243d86bb80b23e30bee80fd
mohammadrafy <mohammadrafy1001@gmail.com>
2025-05-24 23:35:36 +0600

    staged

---
+++ src/pages/ModerationPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ModerationPage = () => {
+  return (
+    <div>
+      <h1>Admin Moderation Page</h1>
+      <p>This is a placeholder for the admin moderation content.</p>
+    </div>
+  );
+};
+
+export default ModerationPage;
